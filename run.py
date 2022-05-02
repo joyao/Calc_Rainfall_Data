b62439a4c8e679cd2a9c944d0e4a3275df6dd530
JYHsu <hsujoyao@gmail.com>
2022-05-02 12:32:34 +0800

    feat: Add create voronoi functions

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 import requests
 import shapefile
 import csv
+import os
+import time
 import geopandas as gpd
 from bs4 import BeautifulSoup
 from urllib.parse import quote, urlencode
+from shapely.ops import unary_union
+from geovoronoi import voronoi_regions_from_coords, points_to_coords
 
 
 def run():
@@ -12,6 +16,7 @@ def run():
         all_stations, ["嘉義縣", "嘉義市", "臺南市"])
     create_shapefile(filtered_stations, headings)
     get_rain_monthly_data(filtered_stations, ["2019", "2020", "2021"])
+    create_voronoi_shape()
 
 
 def get_station_list():
@@ -47,13 +52,13 @@ def create_shapefile(point_list, headings):
     SHP_FILENAME = "rain_station"
     point_list_noheading = [[coord[1] for coord in pair]
                             for pair in point_list]
-    w = shapefile.Writer('./Data/' + SHP_FILENAME + '.shp', shapefile.POINT)
+    w = shapefile.Writer('./Data/' + SHP_FILENAME + '.shp',
+                         shapefile.POINT, encoding="utf8")
     for j in headings:
         w.field(j, 'C', '100')
     for i in point_list_noheading:
         w.point(float(i[3]), float(i[4]))
         w.record(*i)
-        # w.save('./rain_station.shp')
     # create the PRJ file
     prj = open("./Data/%s.prj" % SHP_FILENAME, "w")
     epsg = 'GEOGCS["WGS 84",'
@@ -68,6 +73,7 @@ def create_shapefile(point_list, headings):
 def get_rain_monthly_data(station_list, year_list):
     YEAR_REPORT_URL = "https://e-service.cwb.gov.tw/HistoryDataQuery/YearDataController.do"
     for station in station_list:
+        datasets = []
         for year in year_list:
             payload = {'command': 'viewMain',
                        'station': station[0][1],
@@ -79,17 +85,17 @@ def get_rain_monthly_data(station_list, year_list):
                 html = r.text
                 soup = BeautifulSoup(html, "html.parser")
                 table = soup.find(id='MyTable')
-                headings = [th.get_text()
-                            for th in table.find("tr", attrs={"class": "third_tr"}).find_all("th")]
-                datasets = []
+                headings = ["Year"] + [th.get_text()
+                                       for th in table.find("tr", attrs={"class": "third_tr"}).find_all("th")]
                 for row in table.find_all("tr")[2:]:
                     dataset = zip(headings, (td.get_text()
                                              for td in row.find_all("td")))
-                    datasets.append(tuple(dataset))
-                datasets = filter(None, datasets)
-                csv_filename = "%s_%s_%s" % (
-                    station[5][1], station[0][1], year)
-                save_dataset_to_file(datasets, headings, csv_filename)
+                    dataset_noempty = tuple(filter(None, tuple(dataset)))
+                    if dataset_noempty:
+                        datasets.append((("Year", year), ) + dataset_noempty)
+        datasets = filter(None, datasets)
+        csv_filename = "%s_%s" % (station[5][1], station[0][1])
+        save_dataset_to_file(datasets, headings, csv_filename)
 
 
 def save_dataset_to_file(dataset, heading, filename):
@@ -102,11 +108,52 @@ def save_dataset_to_file(dataset, heading, filename):
 
 
 def create_voronoi_shape(point_shp_filename="./Data/rain_station.shp",
-                         boundary_shp_filename="./Data/reservior/reservior.shp"):
-    gdf = gpd.read_file(point_shp_filename)
+                         boundary_shp_filename="./Data/county_moi/COUNTY_MOI_1090820_clip.shp",
+                         voronoi_shp_filename="./Data/voronoi_%s.shp" % time.strftime("%Y%m%d%H%M%S", time.localtime())):
+    #  boundary_shp_filename="./Data/reservior/reservior_clip.shp"):
+    gdf = gpd.read_file(point_shp_filename, encoding='utf8')
+    gdf = gdf.to_crs(epsg=3826)
     gdf.head()
 
-    boundary = gpd.read_file(boundary_shp_filename)
+    boundary = gpd.read_file(boundary_shp_filename, encoding='utf8')
+    boundary = boundary.to_crs(epsg=3826)
+    gdf_proj = gdf.to_crs(boundary.crs)
+    boundary_shape = unary_union(boundary.geometry)
+    coords = points_to_coords(gdf_proj.geometry)
+
+    # Calculate Voronoi Regions
+    try:
+        region_polys, region_pts = voronoi_regions_from_coords(
+            coords, boundary_shape)
+        gs = gpd.GeoSeries(region_polys)
+        d = {'StationID': gdf["站號"], 'geometry': gs}
+        gdf = gpd.GeoDataFrame(d, crs="EPSG:3826")
+        gdf.to_file(voronoi_shp_filename)
+    except:
+        timestemp = time.strftime("%Y%m%d%H%M%S", time.localtime())
+        clip_shp_path = "%s_%s.shp" % (
+            os.path.splitext(gdf)[0] + "_clip", timestemp)
+        clip_shapefile(
+            point_shp_filename, boundary_shp_filename, clip_shp_path)
+        create_voronoi_shape(
+            clip_shp_path, boundary_shp_filename, voronoi_shp_filename)
+    mask_voromoi_with_reservior(voronoi_shp_filename)
+
+
+def mask_voromoi_with_reservior(voromoi_shp_path, reservior_shp_path="./Data/reservior/reservior_clip.shp"):
+    timestemp = time.strftime("%Y%m%d%H%M%S", time.localtime())
+    final_voronoi_shape_path = "%s_%s.shp" % (
+        os.path.splitext(voromoi_shp_path)[0] + "_final", timestemp)
+    clip_shapefile(
+        voromoi_shp_path, reservior_shp_path, final_voronoi_shape_path)
+
+
+def clip_shapefile(gdf, mask, output_name):
+    shp = gpd.read_file(gdf).to_crs(epsg=3826)
+    mask_shp = gpd.read_file(mask).to_crs(epsg=3826)
+    clip_shp = gpd.clip(shp, mask_shp)
+    clip_shp.to_file(output_name)
+    return output_name
 
 
 if __name__ == '__main__':
